40ea46f9249c846c8de37292276314d26ba71c69
Listiananda Apriliawan <nanda-l.apriliawan@aia.com>
2023-10-12 14:34:50 +0800

    update rendering logic

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-
-# put current date as yyyy-mm-dd HH:MM:SS in $date
+put current date as yyyy-mm-dd HH:MM:SS in $date
 current_date=$(date '+%Y-%m-%d %H:%M:%S')
 
 info="Activity on $current_date"
@@ -12,6 +11,14 @@ case $os in
 
     Darwin)
         cd "`dirname $0`" || exit 1
+
+         # Create a new folder named "rendered in $month $year" inside the folder "generated"
+        month=$(echo $current_date | cut -d '-' -f 2)
+        year=$(echo $current_date | cut -d '-' -f 1)
+        folder_name="rendered in $month $year"
+        mkdir -p "generated/$folder_name"
+        cd "generated/$folder_name"
+
         ;;
 
     *)
@@ -23,8 +30,9 @@ echo "$info" >> rendered.txt
 echo "$info"
 echo
 
+
 git add rendered.txt
 git commit -m "$info :four_leaf_clover:"
-git push -u origin main 
+git push -u origin main
 
 cd -
